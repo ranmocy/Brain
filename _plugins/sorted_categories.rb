0a64b01388344c058b7bcc8ca1b0a7ae1d0773b5
Ranmocy Sheng <ranmocy@gmail.com>
2013-08-07 08:07:42 +0800

    use hashie to refactory grouped_categories

--- _plugins/sorted_categories.rb
+++ _plugins/sorted_categories.rb
@@ -1,3 +1,5 @@
+require 'hashie'
+
 module Jekyll
   class SortedCategoriesBuilder < Generator
 
@@ -5,18 +7,23 @@ module Jekyll
     priority :high
 
     def generate(site)
-      site.config['sorted_categories'] = site.categories.map { |category_name, posts|
-        [ category_name, posts, posts.size, site.config['motto'][category_name].to_s ]
-      }.sort { |a,b| a[0] <=> b[0] }
+      # site.config['sorted_categories'] = site.categories.map { |category_name, posts|
+      #   [ category_name, posts, posts.size, site.config['motto'][category_name].to_s ]
+      # }.sort { |a,b| a[0] <=> b[0] }
 
       site.config['grouped_categories'] = site.config['categories_group'].collect { |group|
-        [
-          group.first,
-          group.last.collect { |category_name|
+        Hashie::Mash.new(
+          name: group.first,
+          categories: group.last.collect { |category_name|
             posts = site.categories[category_name]
-            [ category_name, posts, posts.size, site.config['motto'][category_name].to_s ]
+            Hashie::Mash.new(
+              name: category_name,
+              posts: posts,
+              size: posts.size,
+              motto: site.config['motto'][category_name].to_s
+            )
           }
-        ]
+        )
       }
     end
 
