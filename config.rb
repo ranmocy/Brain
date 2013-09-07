32a6a9a2ba78ac176c40e42f0dc9e6fd2e36a2d0
Ranmocy Sheng <ranmocy@gmail.com>
2013-09-06 23:58:17 -0700

    move source to _site

--- config.rb
+++ config.rb
@@ -76,8 +76,9 @@ page "/feed.xml", :layout => false
 #   end
 # end
 
-# set :source, '.site'
-# set :build, '.built'
+set :source, "_site"
+set :build_dir, '_built'
+set :partials_dir, '_templates'
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
@@ -88,16 +89,16 @@ activate :directory_indexes
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
